8222be14c2316bbc5ad3dd3656712e04cc3f57ff
Krishna Kant Chaudhary <krishnakantchaudhary431@gmail.com>
2026-09-13 01:33:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = 8080;
+const Listing = require("./models/listing.js");
+const path = require('path');
+const methodoverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressErr = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
+
+
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodoverride('_method'));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+main()
+  .then(() => {
+    console.log("connected to database")
+  }).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  console.log(result);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message.join(','));
+    throw new ExpressErr(404, errMsg);
+  } else {
+    next();
+  }
+}
+// NEW ROOUTE
+app.get("/listings/new", (req, res) => {
+  res.render("./listings/new.ejs");
+});
+
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
+  //  let{title , description , image , price , country, location} = req.body;
+  const allListing = await new Listing(req.body.listing);
+  await allListing.save();
+  res.redirect("/listings");
+}));
+
+// SHOW ROUTE
+app.get("/listing/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const allListing = await Listing.findById(id);
+  res.render("./listings/show.ejs", { allListing });
+}));
+
+// EDIT FIELD
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+}));
+
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings`);
+}));
+
+// DELETE ROUTE
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+}));
+
+app.get("/listings", wrapAsync(async (req, res) => {
+  const allListing = await Listing.find({});
+  res.render("./listings/index.ejs", { allListing });
+}));
+
+app.get('/', (req, res) => {
+  res.send("hi i am root");
+});
+
+// FOR ALL TYPE OF ROUTES
+app.all('/*splat', (req, res, next) => {
+  next(new ExpressErr(404, "page not found"));
+});
+
+// //  FOR ERROR HANDLING
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "please provide the required info" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  //  res.status(statusCode).send(message);
+})
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+})
---
+++ utils/ExpressError.js
@@ -0,0 +1,10 @@
+class ExpressErr extends Error{
+    constructor(status , message){
+        super();
+        this.status = status;
+        this.message = message;
+    }
+};
+
+
+module.exports = ExpressErr;
